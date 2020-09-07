8309f4958df45d6ad0aea4b62d4af0cee488e96a
Erian <70418490+Erian-A@users.noreply.github.com>
2020-09-06 21:28:40 -0300

    Add files via upload

---
+++ BST.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct noDaArvore* Apontador;
+typedef struct noDaArvore{
+
+    int chave;
+    Apontador filhoEsq, filhoDir;
+
+} noDaArvore;
+
+
+/// ///
+
+
+
+
+
+
+
+/*
+void Ordem(Apontador *noTree);
+
+void PreOrdem(Apontador *noTree);
+
+void PosOrdem(Apontador *noTree);
+*/
